e19e00618551649f8b10b966dd955ddc24d98660
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-03-23 10:44:28 +0200

    Create owner and species table
    Add foreign keys to animals table to reference ids on owner and species table

--- schema.sql
+++ schema.sql
@@ -13,4 +13,19 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD species varchar(255);
 
+-- Create owners table
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(255),
+    age INT
+);
+
+-- Create Species table
+DROP TABLE IF EXISTS species;
+CREATE TABLE species(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255)
+);
 
