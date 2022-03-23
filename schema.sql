728b3b16f9f9a5b3d885228512eb852bcfad962e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-03-23 10:50:15 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -29,3 +29,16 @@ CREATE TABLE species(
     name VARCHAR(255)
 );
 
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+ALTER TABLE animals DROP species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT species_id FOREIGN KEY (species_id) REFERENCES species (id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD  owners_id INT;
+ALTER TABLE animals ADD CONSTRAINT owners_id FOREIGN KEY (owners_id) REFERENCES owners(id);
+
